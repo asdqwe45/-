301b0bdaee0e88347b239094c1e9369c66d49810
김태빈 <rlaxoqls1998@naver.com>
2023-07-28 17:25:06 +0900

    [FE] feature-유기견 페이지 API 호출
    #145
    김태빈 : 유기견 목록 페이지 API 호출 요청 진행중입니다.

--- front-end/test/src/components/straydog/Straydog.js
+++ front-end/test/src/components/straydog/Straydog.js
@@ -1,20 +1,42 @@
 import { Pagination } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
-import straydogdata from './straydogdata.json'
+import axios from 'axios';
 import { Link } from 'react-router-dom'
 import './Straydog.css';
 
 const dogImages = [
-    ["/dog1.jpg", "/dog2.jpg", "/dog3.jpg", "/dog4.jpg", "/dog5.jpg", "/dog6.jpg", "/dog2.jpg", "/dog3.jpg", "/dog5.jpg", "/dog1.jpg", "/dog2.jpg", "/dog4.jpg", "/dog5.jpg", "/dog1.jpg", "/dog5.jpg", "/dog6.jpg"],
+    ["/dog1.jpg", "/dog2.jpg", "/dog3.jpg", "/dog4.jpg", "/dog5.jpg", "/dog6.jpg", "/dog2.jpg", "/dog3.jpg", "/dog5.jpg", "/dog1.jpg", "/dog2.jpg", "/dog4.jpg", "/dog5.jpg", "/dog1.jpg", "/dog5.jpg"],
     ["/dog1.jpg", "/dog2.jpg", "/dog3.jpg", "/dog4.jpg", "/dog5.jpg", "/dog6.jpg", "/dog4.jpg", "/dog3.jpg", "/dog4.jpg", "/dog1.jpg", "/dog1.jpg", "/dog2.jpg", "/dog3.jpg", "/dog4.jpg", "/dog5.jpg", "/dog6.jpg"],
     // 추가 이미지 배열
 ];
 
-
-
 const Straydog = () => {
     const [currentPage, setCurrentPage] = useState(0);
-    console.log(straydogdata[1].Sex)
+    const [Dogs, setDogs] = useState([]);
+    useEffect(() => {
+        const apiCall = async () => {
+        const response = await axios.get('/dogs');
+        console.log(response.data, '++++');   
+        setDogs(response.data.dogs)
+          
+        };
+        apiCall();
+      }, [])
+    
+    console.log('---')
+    console.log(Dogs)
+
+    const filteredDogs = Dogs.filter(Dog => {
+        return Dog.Status === 'Stray';
+      });
+      //     [{id: 1, name: 'Alice', country: 'Canada'},
+      //     {id: 3, name: 'Carl', 'country: 'Canada'}]
+    console.log(filteredDogs, '*****');
+    
+    const dogImages1 = filteredDogs.map(dog => {
+        return dog.Image
+    })
+    console.log(dogImages1)
 
     return (
 
@@ -25,13 +47,13 @@ const Straydog = () => {
             <h1 style={{ marginTop: '140px' }}>입양견 목록 페이지 입니다.</h1>
 
 
-            <table style={{ marginTop: '30px' }}>
+            <table style={{ marginTop: '140px' }}>
                 <tbody>
                     {Array.from({ length: 4 }).map((_, rowIndex) => (
                         <tr key={rowIndex}>
-                            {dogImages[currentPage].slice(rowIndex * 4, rowIndex * 4 + 4).map((dogImage, index) => (
+                            {filteredDogs[currentPage].slice(rowIndex * 4, rowIndex * 4 + 4).map((dogImage, index) => (
                                 <td key={index}>
-
+                                    
                                     <div class="flip">
                                         <div class="card">
                                             {/* <!-- 앞면 --> */}
@@ -39,6 +61,7 @@ const Straydog = () => {
                                                 <Link to="/straydog-detail" className="nav-link active">
                                                     <img src={dogImage} alt={`Dog ${currentPage * 16 + rowIndex * 4 + index + 1}`} width="300" height="300" />
                                                 </Link>
+                                                
                                             </div>
                                             {/* <!-- 뒷면 --> */}
                                             <div class="back">
