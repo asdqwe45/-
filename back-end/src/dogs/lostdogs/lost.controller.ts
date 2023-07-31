536d2377ceb2bd1fcc38027cf105e3ef75bfd350
이재명 <iamwoaud1004@naver.com>
2023-07-31 16:31:56 +0900

    [BE] feature - lostdog 페이지네이션 구현 # 167 /lostdog?page={page}&pageSize={pageSize} 페이지네이션 기능을 구현하였습니다.

---
+++ back-end/src/dogs/lostdogs/lost.controller.ts
@@ -0,0 +1,38 @@
+import { Query, Controller, Get, Param, Delete, Put, Patch,Post,Body } from '@nestjs/common';
+import { LostDogsService } from './lost.service';
+import { UpdateDogDto } from 'src/dogs/DTO/update.dog.dto';
+
+@Controller('lostdog')
+export class LostDogsController {
+  constructor(
+    private readonly lostDogsService: LostDogsService
+  ) {}
+  @Get()
+  async getDogs(@Query('page') page: number = 1, @Query('pageSize') pageSize:number = 10):Promise<any> {
+    const strayDogs = await this.lostDogsService.getAllLostDogs();
+    if(isNaN(page)||isNaN(pageSize)){
+      page=1;
+      pageSize=10;
+    }
+    const startIndex = (page-1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const paginationStrayDogs = strayDogs.slice(startIndex,endIndex);
+    return paginationStrayDogs;
+  }
+  @Get(':id')
+  getOneLostDog(@Param('id') ID: number)  {
+    return this.lostDogsService.getOneLostDog(ID);
+  }
+  @Delete('/:id')
+  deleteOne(@Param('id') ID: number): Promise<any> {
+    return this.lostDogsService.deleteOne(ID);
+  }
+  @Post()
+  create(@Body() dogData) {
+    this.lostDogsService.create(dogData);
+  }
+  @Patch('/:id')
+  patch(@Param('id') DogID : number, @Body() updateData: UpdateDogDto) {
+    this.lostDogsService.update(DogID,updateData);
+  }
+}
