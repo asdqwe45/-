1a657b039755e454f645b70d44d34346b4f77f20
이재명 <iamwoaud1004@naver.com>
2023-07-31 11:38:08 +0900

    [BE] feature - dog API 구현 # 81 /dogs , /lostdogs , /straydogs의 모든  API 기능 구현

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,14 +3,18 @@ import { DogsModule } from './dogs/dogs.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import  ormconfig from '../ormconfig';
 import { ConfigModule } from '@nestjs/config';
+import { StrayDogsModule } from './straydogs/straydogs.module';
+import { LostDogsModule } from './lost/lost.module';
 
 @Module({
   imports: [
     DogsModule,
+    StrayDogsModule,
     ConfigModule.forRoot({
       envFilePath: ['./development.env'],
     }),
     TypeOrmModule.forRoot(ormconfig),
+    LostDogsModule,
   ],
   controllers: [],
   providers: [],
