448068b22f3570304605079f4c881337fd4adda2
Ubuntu <ubuntu@ip-172-26-2-194.ap-northeast-2.compute.internal>
2023-08-11 09:40:10 +0900

    [BE] fix -server 관련 수정 이민규: 서버 관련 수정 및 업데이트 하였습니다.

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -18,13 +18,19 @@ import { PlayModule } from './play/play.module';
 import { WebsocketModule } from './websocket/websocket.module';
 import * as express from 'express';
 import { join } from 'path';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
   imports: [
+
+    // 서버용
     // ServeStaticModule.forRoot({
-    //   rootPath:join(__dirname,'..','../../front-end/build')
+    //   rootPath: join(__dirname, '..', '../public')
     // }),
-    
+    // 서버 빌드테스트
+    ServeStaticModule.forRoot({
+      rootPath:join(__dirname,'..','../../front-end/build')
+    }),
+
     CacheModule.register({
       isGlobal: true,
       ttl: 60,
